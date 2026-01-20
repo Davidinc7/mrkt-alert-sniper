4e0620efab9765664ed4eac8cf65fd87173bfe65
Davidinc7 <textdrive.agency@gmail.com>
2026-01-21 00:57:59 +0500

    Refactor server setup and logging in index.js

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const http = require("http");
+// index.js
+console.log('[SNIPER] service started');
+
+// Railway o‘chirmasligi uchun kichik server
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('OK');
+});
 
 const PORT = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Mrkt Alert Sniper is running");
-}).listen(PORT, () => {
-  console.log("Server running on port " + PORT);
+server.listen(PORT, () => {
+  console.log('[SNIPER] listening on port', PORT);
 });
 
